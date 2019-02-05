d262184c12217ebdfd3bc8f4a3fccc99496fcaa2
Roberto Gonzalez Perez <rgope@gamma27.aulas.gsyc.urjc.es>
2019-02-05 12:50:05 +0100

    Subo las tablas de multiplicar.

---
+++ tablas_multiplicar.py
@@ -0,0 +1,7 @@
+for numero1 in range(1, 11):
+
+    print('\n', "Tabla del " + str(numero1))
+    print("--------------")
+
+    for numero2 in range(1, 11):
+        print(str(numero1) + " por " + str(numero2) + " es " + str(numero1 * numero2))
